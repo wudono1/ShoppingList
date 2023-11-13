e2905950e4fe65f598ab75cea6a06ba34e408eca
wxz472 <wxz472@case.edu>
2023-11-13 14:14:39 -0500

    amazon parser has multiple outputs

--- WebScraper/amazonParser.py
+++ WebScraper/amazonParser.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import coverage
-#Import the code coverage class so data can be run when this class is called
+
+# Import the code coverage class so data can be run when this class is called
 cov = coverage.Coverage()
 cov.start()
 
@@ -9,91 +10,95 @@ user_input = input("Please enter an item: ")
 
 # Function to extract Product Title
 def get_title(soup):
-	
-	try:
-		# Outer Tag Object
-		title = soup.find("span", attrs={"class":'a-size-base-plus a-color-base a-text-normal'})
+    try:
+        # Outer Tag Object
+        title = soup.find("span", id={"productTitle"}, attrs={"class": 'a-size-large product-title-word-break'})
 
-		# Inner NavigableString Object
-		title_value = title.string
+        # Inner NavigableString Object
+        title_value = title.string
 
-		# Title as a string value
-		title_string = title_value.strip()
+        # Title as a string value
+        title_string = title_value.strip()
 
-		# # Printing types of values for efficient understanding
-		# print(type(title))
-		# print(type(title_value))
-		# print(type(title_string))
-		# print()
+    except AttributeError:
+        title_string = ""
 
-	except AttributeError:
-		title_string = ""	
-
-	return title_string
+    return title_string
 
 # Function to extract Product Price
 def get_price(soup):
+    try:
+        price = soup.find("span", attrs={'class': 'aok-offscreen'}).string.strip()
 
-	try:
-		price = soup.find("span", attrs={'class':'a-offscreen'}).string.strip()
+    except AttributeError:
+        price = ""
 
-	except AttributeError:
-		price = ""	
+    return price
 
-	return price
-
-# Function to extract Product Rating
+''' Function to extract Product Rating
 def get_rating(soup):
+    try:
+        rating = soup.find("span", attrs={'class': 'a-size-base a-color-base'}).string.strip()
 
-	try:
-		rating = soup.find("i", attrs={'class':'a-icon a-icon-star a-star-4-5'}).string.strip()
-		
-	except AttributeError:
-		
-		try:
-			rating = soup.find("span", attrs={'class':'a-icon-alt'}).string.strip()
-		except:
-			rating = ""	
+    except AttributeError:
+        try:
+            rating = soup.find("span", attrs={'class': 'a-icon a-icon-star a-star-4-5'}).string.strip()
+        except:
+            rating = ""
 
-	return rating
+    return rating
+    '''
 
-# Function to extract Number of User Reviews
+'''Function to extract Number of User Reviews
 def get_review_count(soup):
-	try:
-		review_count = soup.find("span", attrs={'class':'a-size-base s-underline-text'}).string.strip()
-		
-	except AttributeError:
-		review_count = ""	
+    try:
+        review_count = soup.find("span", attrs={'class': 'a-size-base'}).string.strip()
 
-	return review_count
+    except AttributeError:
+        review_count = ""
+
+    return review_count
+    '''
 
 if __name__ == '__main__':
+    search_url = "https://www.amazon.com" + f"/s?k={user_input.replace(' ', '+')}"
+    # Headers for request
+    HEADERS = ({
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+        'Accept-Language': 'en-US, en;q=0.5'
+    })
 
-	search_url = "https://www.amazon.com" + f"/s?k={user_input.replace(' ', '+')}"
-	# Headers for request
-	HEADERS = ({'User-Agent':
-	            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-	            'Accept-Language': 'en-US, en;q=0.5'})
+    # HTTP Request
+    webpage = requests.get(search_url, headers=HEADERS)
 
-	# The webpage URL
-	URL = search_url
+    # Soup Object containing all data
+    soup = BeautifulSoup(webpage.content, "lxml")
 
-	# HTTP Request
-	webpage = requests.get(URL, headers=HEADERS)
+    # Fetch links as List of Tag Objects
+    links = soup.find_all("a", attrs={'class': 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'})
 
-	# Soup Object containing all data
-	soup = BeautifulSoup(webpage.content, "lxml")
+    # Store the links
+    links_list = []
 
-	# Function calls to display all necessary product information
-	print("Product Title =", get_title(soup))
-	print("Product Price =", get_price(soup))
-	print("Product Rating =", get_rating(soup))
-	print("Number of Product Reviews =", get_review_count(soup))
-	print(URL)
-	print()
-	print()
+    # Loop for extracting links from Tag Objects
+    for link in links:
+        if len(links_list) >= 10:
+            break  # Stop adding links if the list size is already 10
+        links_list.append(link.get('href'))
 
-	cov.stop()
-	cov.save()
+    for link in links_list:
+        new_webpage = requests.get("https://www.amazon.com" + link, headers=HEADERS)
+        new_soup = BeautifulSoup(new_webpage.content, "html.parser")
+
+        # Function calls to display all necessary product information
+        print("Product Title =", get_title(new_soup))
+        print("Product Price =", get_price(new_soup))
+        #print("Product Rating =", get_rating(new_soup))
+        #print("Number of Product Reviews =", get_review_count(new_soup))
+        print()
+        print()
+
+    cov.stop()
+    cov.save()
     # Generate and print a coverage report
-	cov.report()
+    cov.report()
