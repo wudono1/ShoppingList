5627886ba4e5a95f603fbdcce57aa2744453fbd9
wudono1 <117419158+wudono1@users.noreply.github.com>
2023-11-20 22:40:44 -0500

    alibaba parser changes

--- WebScraper/alibabaParser.py
+++ WebScraper/alibabaParser.py
@@ -10,61 +10,38 @@ user_input = input("Please enter an item: ")
 
 def getLinkAndPrice(item):
     # The webpage URL
-    URL = "https://www.alibaba.com" + f"/trade/search?spm=a2700.galleryofferlist.pageModule_fy23_pc_search_bar.keydown__Enter&tab=all&searchText={item.replace(' ', '+')}"
+	URL = "https://www.alibaba.com" + f"/trade/search?spm=a2700.galleryofferlist.pageModule_fy23_pc_search_bar.keydown__Enter&tab=all&searchText={item.replace(' ', '+')}"
     
     # Headers for request
-    HEADERS = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-        'Accept-Language': 'en-US, en;q=0.5'
-    }
+	HEADERS = {
+		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+		'Accept-Language': 'en-US, en;q=0.5'
+	}
 
-    # HTTP Request
-    webpage = requests.get(URL, headers=HEADERS)
+	# HTTP Request
+	webpage = requests.get(URL, headers=HEADERS)
 
-    data = []
+	data = []
 
-    # Soup Object containing all data
-    soup = BeautifulSoup(webpage.content, "lxml")
+	# Soup Object containing all data
+	soup = BeautifulSoup(webpage.content, "lxml")
 
 
-    for item_elem in soup.select(".card-info"):
-        title_element = item_elem.select_one('.search-card-e-title a')
+	for item_elem in soup.select(".card-info"):
+		title_element = item_elem.select_one('.search-card-e-title a')
 		
-        if title_element:
-            title = title_element.get_text(strip=True)
-            link = item_elem.select_one('.search-card-e-detail-wrapper')['href']
+		if title_element:
+			title = title_element.get_text(strip=True)
+			link = item_elem.select_one('.search-card-e-detail-wrapper')['href']
             
-            try:
-                price = item_elem.select_one('.search-card-e-price-main').text
-            except:
-                price = None
-        data.append({
+			try:
+				price = item_elem.select_one('.search-card-e-price-main').text
+			except:
+				price = None
+		data.append({
 			'item': {'title': title, 'link': link, 'price': price}
 		})
-        print(json.dumps(data, indent=2, ensure_ascii=False))
-		
-
-# Function to extract Product Price
-def get_price(soup):
-
-	try:
-		price = soup.find('div', class_='search-card-e-price-main').string.strip()
-
-	except AttributeError:
-		price = ""	
-
-	return price
-
-# Function to extract Product url
-def get_url(soup):
-	try:
-		url_a = soup.find('a', class_='search-card-e-slider__link search-card-e-slider__gallery')
-		url_value = url_a.get('href') if url_a else None
-		
-	except AttributeError:
-		url_value = ""	
-
-	return url_value
+		print(json.dumps(data, indent=2, ensure_ascii=False))
 
 if __name__ == '__main__':
 
