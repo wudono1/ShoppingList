9eba2599cca01b23d65577fccb9c41d01b9b88ad
wudono1 <117419158+wudono1@users.noreply.github.com>
2023-12-11 01:06:50 -0500

    index logic: saves input items; displays them when returned to page

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', () => {
+    loadItems(); //loads any items previously saved in shopping list tab
 
     //item form
     const item_form = document.querySelector('#new-item-form');
@@ -112,6 +113,8 @@ window.addEventListener('load', () => {
             })
         }
 
+        saveItems();
+
     })
 
     budget_form.addEventListener('submit', (e) => {
@@ -167,9 +170,129 @@ window.addEventListener('load', () => {
     
         if (result.success) {
             console.log('File saved successfully:', result.filename);
+            localStorage.setItem('finalShoppingList', jsonData);
             window.location.href = '/result.html';
         } else {
             console.error('Error saving file.');
         }
     });
 });
+
+
+function saveItems() { //save items for when page is loaded back
+    const items = [];
+    const itemList = document.querySelectorAll('.item');
+    itemList.forEach((itemElement) => {
+        const text = itemElement.querySelector('.text').value;
+        const priority = itemElement.querySelector('.dropdown').value;
+        items.push({ text, priority });
+    });
+    localStorage.setItem('items', JSON.stringify(items));
+}
+
+function loadItems() {
+    const savedItems = JSON.parse(localStorage.getItem('items')) || [];
+    savedItems.forEach(itemData => {
+        addItemToPage(itemData.text, itemData.priority);
+    });
+    
+    // Load and set the budget
+    const savedBudget = localStorage.getItem('budget');
+    if (savedBudget) {
+        document.getElementById('new-budget-input').value = savedBudget;
+    }
+}
+
+function addItemToPage(text, priority) {
+    const list_el = document.querySelector('#items');
+    
+    // Create item element
+    const item_el = document.createElement("div");
+    item_el.classList.add("item");
+
+    // Create item content (item text)
+    const item_content_el = document.createElement("div");
+    item_content_el.classList.add("content");
+
+    // Add item content to item_el element
+    item_el.appendChild(item_content_el);
+
+    // Input for item text
+    const item_input_el = document.createElement("input");
+    item_input_el.classList.add("text");
+    item_input_el.type = "text";
+    item_input_el.value = text;
+    item_input_el.setAttribute("readonly", "readonly");
+
+    item_content_el.appendChild(item_input_el);
+
+    // Label for priority dropdown
+    const label_el = document.createElement("label");
+    label_el.textContent = "Select priority level:";
+
+    // Priority dropdown element
+    const dropdown_el = document.createElement("select");
+    dropdown_el.classList.add("dropdown");
+
+    // Options: high, medium, or low priority
+    const optionHigh = document.createElement("option");
+    optionHigh.value = "high";
+    optionHigh.text = "High";
+    dropdown_el.appendChild(optionHigh);
+
+    const optionMedium = document.createElement("option");
+    optionMedium.value = "med";
+    optionMedium.text = "Medium";
+    dropdown_el.appendChild(optionMedium);
+
+    const optionLow = document.createElement("option");
+    optionLow.value = "low";
+    optionLow.text = "Low";
+    dropdown_el.appendChild(optionLow);
+
+    // Set the dropdown to the saved priority
+    dropdown_el.value = priority;
+
+    // Adding label and dropdown to item_el
+    item_el.appendChild(label_el);
+    item_el.appendChild(dropdown_el);
+
+    // Actions div for edit and delete buttons
+    const item_actions_el = document.createElement("div");
+    item_actions_el.classList.add("actions");
+
+    // Edit button
+    const item_edit_el = document.createElement("button");
+    item_edit_el.classList.add("edit");
+    item_edit_el.innerText = "Edit";
+    item_actions_el.appendChild(item_edit_el);
+
+    // Delete button
+    const item_delete_el = document.createElement("button");
+    item_delete_el.classList.add("delete");
+    item_delete_el.innerText = "Delete";
+    item_actions_el.appendChild(item_delete_el);
+
+    // Append actions to item_el
+    item_el.appendChild(item_actions_el);
+
+    // Append item_el to the list on the page
+    list_el.appendChild(item_el);
+
+    // Logic for edit button
+    item_edit_el.addEventListener('click', () => {
+        if (item_edit_el.innerText.toLowerCase() === "edit") {
+            item_input_el.removeAttribute("readonly");
+            item_input_el.focus();
+            item_edit_el.innerText = "Save";
+        } else {
+            item_input_el.setAttribute("readonly", "readonly");
+            item_edit_el.innerText = "Edit";
+        }
+    });
+
+    // Logic for delete button
+    item_delete_el.addEventListener('click', () => {
+        list_el.removeChild(item_el);
+    });
+}
